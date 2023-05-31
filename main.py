f1d62628994bbec79ef9e7823154b0a918c0b57b
Abhinav Khushalani <ask9908@me.com>
2023-05-31 03:12:02 -0400

    Refactored data storage for better plug-and-play with client credentials vs password auth

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from getpass import getpass
 import utils
 
 if __name__ == '__main__':
-    credentials = utils.check_credentials()
-    if credentials:
-        print('Credentials found!')
-        print(f"Client ID: {credentials['client_id']}\nClient secret: {credentials['client_secret']}")
-    else:
-        print('Credentials not found.')
+    print('Running in password mode...')
 
-        client_id = input("Client ID: ")
-        utils.insert_into_db('client_id', client_id)
-
-        client_secret = getpass(prompt='Client secret: ')
-        utils.insert_into_db('client_secret', client_secret)
+    utils.acquire_and_store_pair('client_id', 'client_secret', public_display='Client ID')
+    utils.acquire_and_store_pair('username', 'password')
--- utils.py
+++ utils.py
@@ -1,18 +1,46 @@
 from tinydb import TinyDB, Query
-
+from dotenv import dotenv_values
+from getpass import getpass
 db = TinyDB('data/credentials.json')
 
 
-def check_credentials():
+def __check_pair(public_name, private_name):
     Entry = Query()
-    client_id = db.search(Entry.key == 'client_id')
-    client_secret = db.search(Entry.key == 'client_secret')
+    public = db.search(Entry.key == public_name)
+    private = db.search(Entry.key == private_name)
 
-    if (client_id and client_secret) and (len(client_id) == 1 and len(client_secret) == 1):
-        return {'client_id': client_id[0]['value'], 'client_secret': client_secret[0]['value']}
+    if (public and private) and (len(public) == 1 and len(private) == 1):
+        return {public_name: public[0]['value'], private_name: private[0]['value']}
     else:
         return None
 
 
-def insert_into_db(key, value):
+def __insert_into_db(key, value):
     db.insert({'key': key, 'value': value})
+
+
+def acquire_and_store_pair(public_name, private_name, public_display=None, private_display=None):
+    public_display = public_display if public_display else public_name.replace("_", " ").title()
+    private_display = private_display if private_display else private_name.replace("_", " ").title()
+
+    print(f"Checking for {public_display} and {private_display}")
+    pair = __check_pair(public_name, private_name)
+    if pair:
+        print('Credentials found!')
+        print(f"{public_display}: {pair[public_name]}\n{private_display}: {pair[private_name]}")
+    else:
+        print('Credentials not found, checking .env file...')
+        dotenv = dotenv_values('.env')
+        if all(name.upper() in dotenv for name in (public_name, private_name)):
+            print('Credentials found!')
+            __insert_into_db(public_name, dotenv[public_name.upper()])
+            __insert_into_db(private_name, dotenv[private_name.upper()])
+            print(f"{public_display}: {dotenv[public_name.upper()]}\n{private_display}: {dotenv[private_name.upper()]}")
+        else:
+            print('Credentials not found in .env file, enter manually or exit.')
+            public = input(f"{public_display}: ")
+            __insert_into_db(public_name, public)
+
+            private = getpass(prompt=f"{private_display}: ")
+            __insert_into_db(private_name, private)
+    print()
